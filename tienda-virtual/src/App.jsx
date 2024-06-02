bbe39ef8418b0ea278bc63c999b0e1b65fee3c4b
Didier Oliva <di.oliva@itecriocuarto.org.ar>
2024-06-02 19:33:45 -0300

    Update React app

--- tienda-virtual/src/App.jsx
+++ tienda-virtual/src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage.jsx';
 import CategoryPage from './pages/CategoryPage.jsx';
 import ProductPage from './pages/ProductPage.jsx';
@@ -8,12 +8,12 @@ import CartPage from './pages/CartPage.jsx';
 
 const App = () => (
   <Router>
-    <Switch>
-      <Route path="/" exact component={HomePage} />
-      <Route path="/category/:name" component={CategoryPage} />
-      <Route path="/product/:id" component={ProductPage} />
-      <Route path="/cart" component={CartPage} />
-    </Switch>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/category/:name" element={<CategoryPage />} />
+      <Route path="/product/:id" element={<ProductPage />} />
+      <Route path="/cart" element={<CartPage />} />
+    </Routes>
   </Router>
 );
 
--- tienda-virtual/src/index.js
+++ tienda-virtual/src/index.js
@@ -1,10 +1,11 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import './index.css';
-import App from './App';
+import App from './App.jsx'; 
 import reportWebVitals from './reportWebVitals';
-import { CartProvider } from './context/CartContext';
+import CartProvider from './context/CartContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
