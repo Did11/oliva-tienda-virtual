d27b02a3a8b5abf459c4a73c1275c17446d7be51
Didier Oliva <di.oliva@itecriocuarto.org.ar>
2024-06-10 21:07:10 -0300

    Fix header

--- tienda-virtual/src/components/Header.jsx
+++ tienda-virtual/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useAuth } from '../context/AuthContext';
-import './Header.css';  // Importa el nuevo archivo de estilos
+import '../styles/Header.css';  // Importa el nuevo archivo de estilos
 
 const Header = () => {
     const [searchTerm, setSearchTerm] = useState('');
